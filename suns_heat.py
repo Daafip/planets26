0e440146f6e087adc424d45298e2e48244ec5471
Daafip <daafips@gmail.com>
2024-02-27 12:42:40 +0100

    fixed bug in code

--- suns_heat.py
+++ suns_heat.py
@@ -5,12 +5,14 @@ def suns_heat(planet_name):
     if planet_name in dict_name_heat:
         return dict_name_heat[planet_name]
     else:
-	return "Unknown planet"
+	    return "Unknown planet"
 
-dict_name_heat = {'earth': 30}
+dict_name_heat = {'earth': 30,
+                  'mars': 60,
+                  'venus': 10}
 
-planet_name = input("Which planet heat do you want?")
-
-print(suns_heat(planet_name))git 
+while True:
+    planet_name = input("Which planet heat do you want? (all lower caps)")
+    print(suns_heat(planet_name))
 
 time.sleep(1)
